e5908f38229ee807af31a32b44247bc395a7fd25
joel T <joel.torib@gmail.com>
2016-11-25 16:07:15 +0100

    joelT

---
+++ pizzeria-console-objet/fr/pizzeria/exception/DeletePizzaException.java
@@ -0,0 +1,10 @@
+package fr.pizzeria.exception;
+
+public class DeletePizzaException extends Exception {
+
+	private static final long serialVersionUID = -205480846048285884L;
+
+		public DeletePizzaException(){
+			
+		}
+}
